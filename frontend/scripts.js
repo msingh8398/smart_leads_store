ba3df525a7e4bff9285446e6c9ecac1213ca9486
mihir <mihirrajput9@gmail.com>
2023-04-07 22:12:52 -0400

    front end changes

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,8 +1,7 @@
 "use strict";
 
 const serverUrl = "http://127.0.0.1:8000";
-// const serverUrl="https://x1sfhszghj.execute-api.us-east-1.amazonaws.com/api/"
-
+var data = [];
 async function uploadImage() {
     // encode input file as base64 string for upload
     let file = document.getElementById("file").files[0];
@@ -37,11 +36,6 @@ async function uploadImage() {
 }
 
 function updateImage(image) {
-    // document.getElementById("view").style.display = "block";
-    // document.getElementById("my_table_id").style.display = "block";
-    // document.getElementById("view").className = "border";
-    
-
     let imageElem = document.getElementById("image");
     imageElem.src = image["fileUrl"];
     imageElem.alt = image["fileId"];
@@ -68,16 +62,55 @@ function extractImageData(image) {
 }
 
 
-function showResult(data) {
+function saveData() {
+    return fetch(serverUrl + "/save-data", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"data":data.entities})
+    }).then(response => {
+        if (response.ok) {
+            alert("Data saved successfully");
+        } else {
+            throw new HttpError(response);
+        }
+    })
+}
+
+function showEditBox(text, id) {
+    $("#myModal").modal('show');
+    document.getElementById("modal_input").value = text;
+    document.getElementById("hiddenData").value = id;
+}
+
+function updateValue() {
+    var text = document.getElementById("modal_input").value;
+    var id = document.getElementById("hiddenData").value;
+
+    for (var i = 0; i < data.entities.length; i++) {
+        if (data.entities[i].Id == id) {
+            data.entities[i].Text = text;
+            break;
+        }
+    }
+    showResult(data)
+    $("#myModal").modal('hide');
+}
+
+function showResult(incoming_data) {
+    data = incoming_data;
     console.log(data.entities);
     var table_row = '';
     data.entities.forEach(element => {
-        table_row += '<tr><td class="col-md-2">'+element.Text+'</td><td class="col-md-2">'+element.Type+'</td><td class="col-md-2">'+element.Score+'</td><td class="col-md-2"><a class="btn btn-dark">Edit</a></td></tr>'
+        table_row += '<tr id="data_'+element.Id+'"><td class="col-md-2">'+element.Text+'</td><td class="col-md-2">'+element.Type+'</td><td class="col-md-2">'+element.Score+'</td><td class="col-md-2"><a class="btn btn-dark" href="#myModal" onclick="showEditBox(\'' + element.Text +'\',\''+ element.Id + '\')">Edit</a></td></tr>'
     });
 
     document.getElementById("table_data").innerHTML = table_row;
     document.getElementById("view").style.display = "block";
     document.getElementById("my_table_id").style.display = "block";
+    document.getElementById("save_btn").style.display = "block";
     document.getElementById("view").className = "border";
 }
 
@@ -85,7 +118,7 @@ function uploadAndTranslate() {
     uploadImage()
         .then(image => updateImage(image))
         .then(image => extractImageData(image))
-        .then(image => showResult(image))
+        .then(data => showResult(data))
         .catch(error => {
             alert("Error: " + error);
         })
@@ -97,4 +130,35 @@ class HttpError extends Error {
         this.name = "HttpError";
         this.response = response;
     }
+}
+
+function getAllLeads() {
+    return fetch(serverUrl + "/get-all-leads", {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new HttpError(response);
+        }
+    })
+}
+
+function showAllLeads(data){
+    console.log(data);
+    document.getElementById("data_container").style.display = "block";
+}
+
+function showAllLeadsData(){
+
+    document.getElementById("main_page").style.display = "none";
+    getAllLeads()
+        .then(data => showAllLeads(data))
+        .catch(error => {
+            alert("Error: " + error);
+        })
 }
